a2f768b1779211aad4aca6a639d8f9057a4fde1e
tstep2009 <122586524+tstep2009@users.noreply.github.com>
2025-07-20 22:01:21 +0000

    Refactor Card class implementation and improve main execution block

--- start/01_01/getting_started.py
+++ start/01_01/getting_started.py
@@ -1,3 +1,22 @@
 """
 Python Object-Oriented Programming Class Example
 """
+
+class Card:
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+    def __repr__(self):
+        return f"{self.value} of {self.suit}"
+    
+if __name__ == "__main__ ":
+    card1 = Card ("Ace", "Spades") # Option shift dwn arrow 
+    card2 = Card ("Queen", "Hearts")
+
+    print(card1)
+    print(card2)
+
+    
+    
+
